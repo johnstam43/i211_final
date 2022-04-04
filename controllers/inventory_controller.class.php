d9156873600c134a38b441f67627135779ba7b4c
elibules <elibules@gmail.com>
2022-04-04 11:31:14 -0400

    Inventory controller and model skeleton finished

--- controllers/inventory_controller.class.php
+++ controllers/inventory_controller.class.php
@@ -1,6 +1,12 @@
 <?php
 class InventoryController
 {
+    private $inventory_model;
+
+    public function __construct()
+    {
+        $this->inventory_model = InventoryModel::getInventoryModel();
+    }
     public function index()
     {
         $view = new InventoryView();
---
+++ models/inventory_model.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class InventoryModel
+{
+    private $db;
+    private $dbConnection;
+    static private $_instance = NULL;
+
+    private function __construct()
+    {
+        $this->db = Database::getDatabase();
+        $this->dbConnection = $this->db->getConnection();
+
+        //Escapes special characters in a string for use in an SQL statement. This stops SQL inject in POST vars. 
+        foreach ($_POST as $key => $value) {
+            $_POST[$key] = $this->dbConnection->real_escape_string($value);
+        }
+
+        //Escapes special characters in a string for use in an SQL statement. This stops SQL Injection in GET vars 
+        foreach ($_GET as $key => $value) {
+            $_GET[$key] = $this->dbConnection->real_escape_string($value);
+        }
+    }
+
+    public static function getInventoryModel()
+    {
+        if (self::$_instance == NULL) {
+            self::$_instance = new InventoryModel();
+        }
+        return self::$_instance;
+    }
+}
